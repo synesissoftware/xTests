d22bcc106abb69a3a32d613069cecb946a129d8a
Matt Wilson <matthew.wilson.synesis@gmail.com>
2024-01-02 17:45:31 +1100

    CMake : ~ renamed target 'xTests' to 'xTests.core' (as in 'xTests::xTests.core')

--- run_all_unit_tests.sh
+++ run_all_unit_tests.sh
@@ -56,11 +56,11 @@ echo "Executing make and then running all test programs"
 
 make && for f in $(find . -type f -perm +111 -print -name 'test_*' -o -name 'test.*')
 do
-	echo
-	echo "executing $f:"
+    echo
+    echo "executing $f:"
 
-	# NOTE: we do not break on fail, because, this being a unit-testing library, the scratch-tests actually fail
-	$f
+    # NOTE: we do not break on fail, because, this being a unit-testing library, the scratch-tests actually fail
+    $f
 done
 
 cd ->/dev/null
