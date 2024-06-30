239ce05fc0185d52beec6fa304dc0049216fe97a
Matt Wilson <matthew@synesis.com.au>
2024-06-30 10:06:03 +1000

    ~ ensuring all headers are installed ~ misc. other fixes

--- include/xtests/xtests.h
+++ include/xtests/xtests.h
@@ -5,7 +5,7 @@
  *          library.
  *
  * Created: 20th June 1999
- * Updated: 5th May 2024
+ * Updated: 30th June 2024
  *
  * Home:    https://github.com/synesissoftware/xTests/
  *
@@ -52,8 +52,8 @@
 #ifndef XTESTS_DOCUMENTATION_SKIP_SECTION
 # define XTESTS_VER_XTESTS_H_XTESTS_MAJOR       3
 # define XTESTS_VER_XTESTS_H_XTESTS_MINOR       41
-# define XTESTS_VER_XTESTS_H_XTESTS_REVISION    16
-# define XTESTS_VER_XTESTS_H_XTESTS_EDIT        357
+# define XTESTS_VER_XTESTS_H_XTESTS_REVISION    17
+# define XTESTS_VER_XTESTS_H_XTESTS_EDIT        358
 #endif /* !XTESTS_DOCUMENTATION_SKIP_SECTION */
 
 
