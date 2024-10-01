ae8ec3111fa07936efb913bb96f037350cc601e9
Matt Wilson <matthew@synesis.com.au>
2024-10-01 17:47:01 +1000

    ~ settings ; ~ CMake boilerplate files

--- run_all_scratch_tests.sh
+++ run_all_scratch_tests.sh
@@ -3,7 +3,7 @@
 ScriptPath=$0
 Dir=$(cd $(dirname "$ScriptPath"); pwd)
 Basename=$(basename "$ScriptPath")
-CMakeDir=$Dir/_build
+CMakeDir=${SIS_CMAKE_BUILD_DIR:-$Dir/_build}
 RunMake=1
 
 
@@ -87,7 +87,7 @@ fi
 
 if [ $status -eq 0 ]; then
 
-    for f in $(find $Dir -type f '(' -name 'test_scratch*' -o -name 'test.scratch.*' -o -name 'test_performance*' -o -name 'test.performance.*' ')' -exec test -x {} \; -print)
+    for f in $(find $CMakeDir -type f '(' -name 'test_scratch*' -o -name 'test.scratch.*' -o -name 'test_performance*' -o -name 'test.performance.*' ')' -exec test -x {} \; -print)
     do
 
         echo
